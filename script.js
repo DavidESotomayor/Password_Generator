42656077e13d5bf55fa1fc135ef82a2c0d6163bf
David Sotomayor <david.elijah.sotomayor@gmail.com>
2020-11-21 01:32:15 -0600

    setting up window prompts

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
+  function generatePassword() {
+    var passwordLength = prompt("How long would you like your password to be?", "Must be between 8 and 128 characters");
+
+    var specialCharacter = prompt("What character types would you like in your password?", "lowercase, uppercase, numeric, special characters");
+
+    
+  }
+
+
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
